41e028085cc5b523d462e1a25e9f7050a4f6e01a
Ziyang Wei <wei.ziya@northeastern.edu>
2021-04-21 21:20:05 -0400

    correlation

--- Scalaproject/src/main/scala/dataAnalysis.scala
+++ Scalaproject/src/main/scala/dataAnalysis.scala
@@ -1,14 +1,16 @@
+import org.apache.spark.mllib.linalg.Vectors
 import org.json4s.scalap.scalasig.Flags
 import org.apache.spark.mllib.stat.Statistics
 import org.apache.spark.sql.SparkSession
 
-class dataAnalysis extends App {
+object dataAnalysis extends App {
 
-  val spark: SparkSession = SparkSession
-    .builder()
-    .appName("ProcessData")
-    .master("local[*]")
-    .getOrCreate()
+  def toDoubleDynamic(x: Any) = x match {
+    case s: String => s.toDouble
+    case jn: java.lang.Number => jn.doubleValue()
+    case _ => throw new ClassCastException("cannot cast to double")
+  }
+
   /**
    * Read and convert data.
    */
@@ -16,33 +18,36 @@ class dataAnalysis extends App {
     val one = row.getAs[Integer]("DBNOs")
     val two = row.getAs[Integer]("assists")
     val three = row.getAs[Integer]("boosts")
-    val four = row.getAs[Float]("damageDealt")
+    //val four = row.getAs[Float]("damageDealt")
     val five = row.getAs[Integer]("headshotKills")
     val six = row.getAs[Integer]("heals")
     val seven = row.getAs[Integer]("killPlace")
-    val eight = row.getAs[Integer]("killPoint")
+    val eight = row.getAs[Integer]("killPoints")
     val nine = row.getAs[Integer]("killStreaks")
     val ten = row.getAs[Integer]("kills")
-    val eleven =row.getAs[Float]("longestKill")
+    //val eleven =row.getAs[Float]("longestKill")
     val twelve = row.getAs[Integer]("matchDuration")
     val onethree = row.getAs[Integer]("rankPoints")
-    val onefour = row.getAs[Integer]("reives")
-    val onefive = row.getAs[Float]("rideDistance")
+    val onefour = row.getAs[Integer]("revives")
+    //val onefive = row.getAs[Float]("rideDistance")
     val onesix = row.getAs[Integer]("roadKills")
-    val oneseven = row.getAs[Float]("swimDistance")
+    //val oneseven = row.getAs[Float]("swimDistance")
     val oneeight = row.getAs[Integer]("teamKills")
     val onenine = row.getAs[Integer]("vehicleDestroys")
-    val twozero = row.getAs[Float]("walkDistance")
+    //val twozero = row.getAs[Float]("walkDistance")
     val twoone = row.getAs[Integer]("weaponsAcquired")
     val twotwo = row.getAs[Integer]("winPoints")
     val twothree = row.getAs[Integer]("numGroups")
-    val twofour = row.getAs[Float]("winPlacePerc")
-
-    /**
-     * Compute correlation
-     */
-
-    val corrMatrix = Statistics.corr(rddpre)
-    corrMatrix.show()
+    //val twofour = row.getAs[Float]("winPlacePerc")
+    Vectors.dense(one.toDouble,two.toDouble,three.toDouble,five.toDouble,six.toDouble,seven.toDouble,eight.toDouble,nine.toDouble,ten.toDouble,twelve.toDouble,onethree.toDouble,onefour.toDouble,onesix.toDouble,oneeight.toDouble,onenine.toDouble,twoone.toDouble,twotwo.toDouble,twothree.toDouble)
   }
+
+
+  /**
+   * Compute correlation
+   */
+
+  val corrMatrix = Statistics.corr(rddpre)
+  println(corrMatrix)
+
 }

e537c30de0e28ad0cad888ecb292ffd70c2e224f
lycoding27 <55034861+lycoding27@users.noreply.github.com>
2021-04-22 03:17:09 -0400

    test2

--- Scalaproject/src/Test/scala/ModelSpec.scala
+++ Scalaproject/src/Test/scala/ModelSpec.scala
@@ -24,53 +24,6 @@ class ModelSpec extends AnyFlatSpec with Matchers {
 
 
 
-//  "DT Model accuracy" should "greater than 0.6" in {
-//    val DTacc = DT.acc
-//    DTacc should be > 0.7
-//  }
-//
-//  "SVM Model accuracy" should "greater than 0.7" in {
-//    val SVMacc = SVM.acc
-//    SVMacc should be > 0.7
-//  }
-//
-//  "SVM coefficients number" should "equal to 9" in {
-//    val SVMcn = SVM.svmModel.coefficients.size
-//    SVMcn should equal(9)
-//  }
-//
-//  "DT features number" should "equal to 9" in {
-//    val DTnf = DT.dtModel.featureImportances.size
-//    DTnf should equal(9)
-//  }
-//
-//  "FM features number" should "equal to 9" in {
-//    val FMnf = FM.fmModel.numFeatures
-//    FMnf should equal(9)
-//  }
-//
-//  "GBT features number" should "equal to 9" in {
-//    val GBTnf = GBT.gbtModel.featureImportances.size
-//    GBTnf should equal(9)
-//  }
-//
-//  "LR coefficients number" should "equal to 9" in {
-//    val LRcn = LR.lrModel.coefficientMatrix.numCols
-//    LRcn should equal(9)
-//  }
-//
-//  "MLP features number" should "equal to 9" in {
-//    val MLPwn = MLP.mlpModel.numFeatures
-//    MLPwn should equal(9)
-//  }
-//
-//  "RF features number" should "equal to 9" in {
-//    val RFnf = RF.rfModel.featureImportances.size
-//    RFnf should equal(9)
-//  }
-//
-//  "best model" should "work for accuracy > 70%" in {
-//    ModelSelection.testAccuracy > 0.7
-//  }
+
 
 }
